3139e677ac43ada701222a8664f0473dd0342749
nirgluzman <nir.gluzman@gmail.com>
2023-05-02 12:12:34 +0200

    add uniform interface for /products

--- server.js
+++ server.js
@@ -13,10 +13,32 @@ HTTP = Hypertext Transfer Protocol
 */
 
 import express from "express";
-import colors from "colors";
+import "colors";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
+
+app.get("/products", (req, res) => {
+  // res.setHeader("Content-Type", "application/json");
+  // res.send(JSON.stringify({ name: "Nir" }));
+  res.json({ msg: "GET all products" });
+});
+
+app.post("/products", (req, res) => {
+  res.json({ msg: "CREATE a new product" });
+});
+
+app.get("/products/:id", (req, res) => {
+  res.json({ msg: "GET a single product product" });
+});
+
+app.put("/products/:id", (req, res) => {
+  res.json({ msg: "REPLACE a single product with a new one" });
+});
+
+app.delete("/products/:id", (req, res) => {
+  res.json({ msg: "DELETE a single product" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`.red);
